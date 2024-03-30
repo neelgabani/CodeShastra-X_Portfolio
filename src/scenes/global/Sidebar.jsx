491e8a21f7b0982a331186fa98cfff28d9347f4c
Neel Gabani <neelgabani24ng@gmail.com>
2024-03-30 23:19:25 +0530

    4 changes

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -176,13 +176,9 @@ const Sidebar = () => {
               setSelected={setSelected}
             />
 
-            <Typography
-              variant="h6"
-              color={colors.grey[300]}
-              sx={{ m: "15px 0 5px 20px" }}
-            >
+          
               {/* Charts */}
-            </Typography>
+            {/* </Typography> */}
             <Item
               // title="Bar Chart"
               to="/bar"
